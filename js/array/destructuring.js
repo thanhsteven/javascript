9aafcca794bd7fbf547271c2a4d4c13895def8da
thanhsteven <kimthanhpham96@gmail.com>
2024-08-22 01:01:40 +0700

    Update source

---
+++ js/array/destructuring.js
@@ -0,0 +1,11 @@
+// * Destructuring Array
+const toys = ["ball", "sword", "arrow", "magic", "water", "fire"];
+const a = toys[0];
+const b = toys[1];
+const c = toys[2];
+
+console.log(a, b, c);
+
+const [x, y, z] = toys;
+console.log([x, y, z]);
+
