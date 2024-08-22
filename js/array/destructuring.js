8dae2f458ee478f6e36e90058903c046f923e458
thanhsteven <kimthanhpham96@gmail.com>
2024-08-22 22:31:23 +0700

    Update source

--- js/array/destructuring.js
+++ js/array/destructuring.js
@@ -7,5 +7,17 @@ const c = toys[2];
 console.log(a, b, c);
 
 const [x, y, z] = toys;
-console.log([x, y, z]);
+// const [ball, sword, arrow] = toys;
+console.log([x, y, z]); // array
+// console.log(ball, sword, arrow); // string
+
+// * Rest Parameters -> các phần tử còn lại
+const [ball, sword, arrow, ...rest] = toys;
+console.log(rest);
+
+// * Rest parameters còn được sử dụng trong function nữa
+function demo(a, ...rest) {
+  console.log(a, rest);
+}
+demo(1, 2, 3, 4, 5);
 
