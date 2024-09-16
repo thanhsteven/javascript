1939c6fb12dd565f46c210c5dc96f05d49d1b914
thanhsteven <kimthanhpham96@gmail.com>
2024-09-16 18:25:09 +0700

    update source

---
+++ DOM/crud.js
@@ -0,0 +1,6 @@
+// ! Thêm, xóa, sửa node trong Javascript
+// * Tạo ra element trong Javscript
+const div = document.createElement("div");
+// * seclector.appendChild
+const body = document.body;
+body.appendChild(div);
