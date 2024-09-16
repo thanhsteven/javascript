126d4ac81321a77e6b9b47563fb2d7d322245d42
thanhsteven <kimthanhpham96@gmail.com>
2024-09-16 15:45:09 +0700

    Update this.js file

---
+++ js/object/this.js
@@ -0,0 +1,15 @@
+// ! this keyword
+// - This : nó sẽ đề cập tới object gần nhất
+const student = {
+  name: "Kim Thành",
+  age: 27,
+  male: true,
+  "last-name": "Phạm",
+  hi: function () {
+    console.log(
+      `Tôi là ${this["last-name"]} ${this.name} và tôi ${this.age} years old !`
+    );
+  },
+};
+
+student.hi();
