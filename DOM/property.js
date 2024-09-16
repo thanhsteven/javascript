8ad747e6f122d78f41c93de3a0e1c593f9339f95
thanhsteven <kimthanhpham96@gmail.com>
2024-09-16 15:39:01 +0700

    Update folder DOM

--- DOM/property.js
+++ DOM/property.js
@@ -1,2 +1,16 @@
 // ! getAttribute("attribute")
 // * attribute -> thuộc tính: href, id, class, src, style
+// * selector : 1 cái
+// * Lấy ra giá trị attribute selector
+
+const link = document.querySelector(".link");
+if (link) {
+  console.log(link.getAttribute("href"));
+}
+
+const li = document.querySelectorAll(".item");
+console.log(li);
+
+li.forEach((item) => {
+  console.log(item.getAttribute("class"));
+});
