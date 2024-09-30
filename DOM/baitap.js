902457ed7e770d190828fc7d2b32ae6ace9bb34f
thanhsteven <kimthanhpham96@gmail.com>
2024-09-30 23:46:55 +0700

    Update source

--- DOM/baitap.js
+++ DOM/baitap.js
@@ -15,13 +15,33 @@ const template = `<div class="modal">
       </div>
     </div>`;
 const body = document.body;
-body.insertAdjacentHTML("afterbegin", template);
+// body.insertAdjacentHTML("afterbegin", template);
+
+// * Cách 3
+const modal = document.createElement("div");
+modal.classList.add("modal");
+
+const modalContent = document.createElement("div");
+modalContent.classList.add("modal-content");
+modal.appendChild(modalContent);
+
+const modalClose = document.createElement("i");
+modalClose.className = "fa fa-times modal-close";
+modalContent.appendChild(modalClose);
+
+const modalDesc = document.createElement("div");
+modalDesc.classList.add("modal-desc");
+modalDesc.textContent =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium impedit praesentium commodi, eum aliquid temporibus! Ab recusandae velit quae, aliquid aperiam sapiente. Eius reprehenderit modi iste explicabo velit perferendis aliquam?";
+modalContent.appendChild(modalDesc);
+
+body.appendChild(modal);
 
 // * Cách 1: Viết code html trực tiếp
-const modal = document.querySelector(".modal");
-if (modal) {
+const modalActive = document.querySelector(".modal");
+if (modalActive) {
   setTimeout(function () {
-    modal.classList.add("is-show");
+    modalActive.classList.add("is-show");
   }, 3000);
 }
 
