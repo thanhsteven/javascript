6fe5e19746e34271cbad34d4cb892779ecacd946
thanhsteven <kimthanhpham96@gmail.com>
2024-10-02 14:55:42 +0700

    update source

--- DOM/final.js
+++ DOM/final.js
@@ -1,9 +1,9 @@
 // ! Viết 1 function tạo ra thông báo
-function createNotification() {
+function createNotification(title) {
   const template = `<div class="noti">
       <img src="./images/thuy-tinh-isoglass.jpg" class="noti-image" />
       <div class="noti-content">
-        <h3 class="noti-title">Welcome to notification</h3>
+        <h3 class="noti-title">${title}</h3>
         <p class="noti-desc">
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
         </p>
@@ -11,7 +11,22 @@ function createNotification() {
     </div>`;
   document.body.insertAdjacentHTML("afterbegin", template);
 }
+
+const randomData = [
+  "Nguyễn Anh Thư",
+  "Nguyễn Huỳnh Minh Châu",
+  "Phạm Kim Thành",
+  "Cao Hoàng Sơn",
+  "Nguyễn Minh Tuấn",
+];
+let lastTitle;
 const timer = setInterval(function () {
-  createNotification();
-}, 4000);
+  const item = document.querySelector(".noti");
+  if (item) item.parentNode.removeChild(item);
+  const title = randomData[Math.floor(Math.random() * randomData.length)];
+  if (lastTitle !== title) {
+    createNotification(title);
+  }
+  lastTitle = title;
+}, 5000);
 
