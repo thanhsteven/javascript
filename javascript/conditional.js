28889d1328d63f8e2727f66e3ea1f5c11a19f06c
thanhsteven <kimthanhpham96@gmail.com>
2024-10-03 13:58:33 +0700

    Update source

--- javascript/conditional.js
+++ javascript/conditional.js
@@ -1,6 +1,6 @@
 // Điều kiện trong Javascript
 const isRich = false;
-const myMoney = 100000000;
+const myMoney = 100;
 if (!isRich) {
   console.log("I will buy a new car");
 } else if (myMoney > 100000) {
@@ -9,23 +9,23 @@ if (!isRich) {
   console.log("I will save my money");
 }
 
-// alert("You have so much money");
-// const yourName = prompt("What is your name ?", "");
-// console.log(yourName);
-// const isYourMoney = confirm(`Is your name ${yourName} ?`);
-// console.log(isYourMoney);
+alert("You have so much money");
+const yourName = prompt("What is your name ?", "");
+console.log(yourName);
+const isYourMoney = confirm(`Is your name ${yourName} ?`);
+console.log(isYourMoney);
 
 // ! Bài 1
 // Todo: Nhập vào số tuổi và kiểm tra nếu số tuổi lớn hơn 18 thì được coi phim rạp và ngược lại thì không được vào xem.
-// const yourAge = prompt("Vui lòng nhập vào số tuổi của bạn ?", "");
-// console.log(typeof yourAge);
-// let message = "";
-// if (Number(yourAge) >= 18) {
-// 	message = "Bạn có thể vào rạp nha !";
-// } else {
-// 	message = "Bạn chưa đủ tuổi nha !";
-// }
-// alert(message);
+const yourAge = prompt("Vui lòng nhập vào số tuổi của bạn ?", "");
+console.log(typeof yourAge);
+let message = "";
+if (Number(yourAge) >= 18) {
+  message = "Bạn có thể vào rạp nha !";
+} else {
+  message = "Bạn chưa đủ tuổi nha !";
+}
+alert(message);
 
 // ! Bài 2
 // Todo: Cho 2 số A và B, tìm ra số lớn hơn.
