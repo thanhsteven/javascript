8e0733471de5b37db3f62eec6af4db654e231dcb
thanhsteven <kimthanhpham96@gmail.com>
2024-10-03 11:58:40 +0700

    Update source

--- kimthanh/number.js
+++ kimthanh/number.js
@@ -53,3 +53,13 @@ console.log(Math.max(numberG, numberJ));
 
 console.log(Number(3.14));
 console.log(Number("123465"));
+
+// * Number(value)
+console.log(Number("4.5")); // = 4.5
+console.log(Number("5")); // = 5
+console.log(Number("abcdef")); // = NaN
+console.log(Number(undefined)); // = NaN
+console.log(Number(null)); // = 0
+console.log(Number(false)); // = 0
+console.log(Number(true)); // = 1
+console.log(Number(NaN)); // = NaN
