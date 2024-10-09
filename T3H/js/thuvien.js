e5b8b5a64fbbd25717bec8fe15d4141c02365557
thanhsteven <kimthanhpham96@gmail.com>
2024-10-09 13:12:27 +0700

    Update

--- T3H/js/thuvien.js
+++ T3H/js/thuvien.js
@@ -2,3 +2,19 @@
 function tongHaiSo(a, b) {
   return Number(a) + Number(b);
 }
+
+// * Hàm kiểm tra input
+function checkInputType(value) {
+  if (typeof value === "string") {
+    console.log("Vui lòng nhập con số");
+  } else if (isNaN(value)) {
+    console.log("Vui lòng nhập số");
+  } else {
+    console.log("Đầu vào hợp lệ");
+  }
+}
+
+// * Kiểm tra số chẳn lẻ
+function checkEvenOdd(number) {
+  return number % 2 == 0;
+}
