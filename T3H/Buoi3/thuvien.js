3f4c841ba3e546d15f42ba4c95b516d3c258b2b9
thanhsteven <kimthanhpham96@gmail.com>
2024-10-11 18:06:39 +0700

    Update source

---
+++ T3H/Buoi3/thuvien.js
@@ -0,0 +1,18 @@
+function chuan_hoa_chuoi(chuoi) {
+  chuoi_tam = chuoi.trim();
+  do {
+    chuoi_tam = chuoi_tam.replace("  ", " ");
+    var vt = chuoi_tam.indexOf("  ");
+  } while (vt >= 0);
+  return chuoi_tam;
+}
+
+let Duong_dan_Du_lieu = "./Du_lieu";
+function Doc_tap_tin(tapTin) {
+  let Dia_chi_Xu_ly = `${Duong_dan_Du_lieu}/${tapTin}`;
+  let Xu_ly_HTTP = new XMLHttpRequest();
+  Xu_ly_HTTP.open(`GET`, `${Dia_chi_Xu_ly}`, false);
+  Xu_ly_HTTP.send();
+  let Chuoi_ket_qua = Xu_ly_HTTP.responseText.trim();
+  return Chuoi_ket_qua;
+}
