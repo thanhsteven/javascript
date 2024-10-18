fbf5a1e956ff53668c35574656a92003b4a6a6c7
thanhsteven <kimthanhpham96@gmail.com>
2024-10-18 17:39:45 +0700

    Update source

--- T3H/Buoi4/app.js
+++ T3H/Buoi4/app.js
@@ -20,3 +20,28 @@ let hv2 = hv1;
 console.log(hv2);
 console.log(hv2.Thongtin());
 console.log(hv2.Hocbong);
+
+// Xây dựng lớp hình chữ nhật
+// Phương thức khởi tạo: thuộc tính DAI, RONG
+// Phương thức tính toán: DIEN_TICH, CHU_VI
+
+class HCN {
+  // Phương thức khởi tạo
+  constructor(_dai, _rong) {
+    this.Dai = _dai;
+    this.Rong = _rong;
+  }
+  // Phương thức tính toán
+  chuvi() {
+    return `Chu vi = ${(this.Dai + this.Rong) * 2}`;
+  }
+  dientich() {
+    return `Diện tích = ${this.Dai * this.Rong}`;
+  }
+  thongtin() {
+    return `Hình chữ nhật có chiều dài là ${this.Dai} và chiều rộng là ${this.Rong}`;
+  }
+}
+
+let hcn_1 = new HCN(6, 4);
+console.log(hcn_1.chuvi() + "\n" + hcn_1.dientich());
--- T3H/js/thuvien.js
+++ T3H/js/thuvien.js
@@ -1,73 +1,21 @@
-// * Tổng hai số
-function tongHaiSo(a, b) {
-  return Number(a) + Number(b);
+// * Hàm Chuẩn hoá chuỗi - thầy
+function chuan_hoa_chuoi(chuoi) {
+  chuoi_tam = chuoi.trim();
+  do {
+    chuoi_tam = chuoi_tam.replace("  ", " ");
+    var vt = chuoi_tam.indexOf("  ");
+  } while (vt >= 0);
+  return chuoi_tam;
 }
 
-// * Hàm kiểm tra input
-function checkInputType(value) {
-  if (typeof value === "string") {
-    console.log("Vui lòng nhập con số");
-  } else if (isNaN(value)) {
-    console.log("Vui lòng nhập số");
-  } else {
-    console.log("Đầu vào hợp lệ");
-  }
-}
+let Duong_dan_Du_lieu = "./Du_lieu";
 
-// * Kiểm tra số chẳn lẻ
-function checkEvenOdd(number) {
-  return number % 2 == 0;
-}
-
-// * Hàm viết hoa chữ cái đầu trong chuỗi
-function capitalizeWord(string) {
-  if (string.length == 0) return false;
-  let strArr = string.split(" ");
-  let filterArr = strArr.filter(function (element) {
-    return element !== "";
-  });
-  let mapArr = filterArr.map(function (element) {
-    let firstChar = element.toLowerCase().charAt(0).slice().toUpperCase();
-    let otherChar = element.toLowerCase().slice(1);
-    return firstChar + otherChar;
-  });
-  return mapArr.join(" ");
-}
-
-// * Hàm viết in hoa toàn bộ
-function uppercaseWord(string) {
-  if (string.length == 0) return false;
-  let arrStr = string.split(" ");
-  let arrFilter = arrStr.filter(function (element) {
-    return element !== "";
-  });
-  let arrMap = arrFilter.map(function (element) {
-    return element.toUpperCase();
-  });
-  return arrMap.join(" ");
-}
-
-// * Hàm viết thường toàn bộ
-function lowerWord(string) {
-  if (string.length == 0) return false;
-  let arrStr = string.split(" ");
-  let arrFilter = arrStr.filter(function (element) {
-    return element !== "";
-  });
-  let arrMap = arrFilter.map(function (element) {
-    return element.toLowerCase();
-  });
-  return arrMap.join(" ");
-}
-
-// * Hàm viết hoa đầu câu
-function captializeSentences(string) {
-  if (string.length == 0) return false;
-  let arrStr = string.split(" ");
-  let arrFilter = arrStr.filter(function (element) {
-    return element !== "";
-  });
-  let firstChar = arrFilter[0].charAt(0).toUpperCase();
-  let otherChar = arrFilter.join(" ").slice(1);
-  return firstChar + otherChar;
+// Đọc dữ liệu
+function Doc_tap_tin(tapTin) {
+  let Dia_chi_Xu_ly = `${Duong_dan_Du_lieu}/${tapTin}`;
+  let Xu_ly_HTTP = new XMLHttpRequest();
+  Xu_ly_HTTP.open(`GET`, `${Dia_chi_Xu_ly}`, false);
+  Xu_ly_HTTP.send();
+  let Chuoi_ket_qua = Xu_ly_HTTP.responseText.trim();
+  return Chuoi_ket_qua;
 }
