756e53ca3f50a492564e0ef8875269b4bac53a99
thanhsteven <kimthanhpham96@gmail.com>
2024-10-24 23:09:01 +0700

    Update source

--- ktcity/event/event.js
+++ ktcity/event/event.js
@@ -1,3 +1,6 @@
 // * 1. Sự kiện (event) là gì ?
 // * 2. Thêm 1 sự kiện: selector.addEventListener("eventName", handler, [options]);
 // * 3. Các sự kiện cơ bản: click, keypress, keyup, keydown, mouseover, mousemove, mouseleave, load, DOMContentLoader,...
+
+// * matches
+// * Kiểm tra xem có khớp hay không
